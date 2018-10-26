1a16e4134091641da77d864bb64c8d3d221103c7
Anton Novojilov <andy@essentialkaos.com>
2018-10-27 01:53:55 +0300

    Added fuzz testing for procfiles parsers

---
+++ procfile/fuzz.go
@@ -0,0 +1,29 @@
+// +build gofuzz
+
+package procfile
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                       Copyright (c) 2006-2018 FB GROUP LLC                         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func FuzzV1(data []byte) int {
+	_, err := parseV1Procfile(data, &Config{})
+
+	if err != nil {
+		return 1
+	}
+
+	return 0
+}
+
+func FuzzV2(data []byte) int {
+	_, err := parseV2Procfile(data, &Config{})
+
+	if err != nil {
+		return 1
+	}
+
+	return 0
+}
